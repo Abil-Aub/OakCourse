f14f92ba00d39b367ce73b084f1c7b0c077f6cfe
Abil AUBAKIROV <abil.aubakirov@gmail.com>
2022-09-30 17:28:15 +0200

    Begin add social media

---
+++ UI/Areas/Admin/Controllers/SocialMediaController.cs
@@ -0,0 +1,26 @@
+﻿using DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SocialMediaController : Controller
+    {
+        // GET: Admin/SocialMedia
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddSocialMedia()
+        {
+            SocialMediaDTO model = new SocialMediaDTO();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult AddSocialMedia(SocialMediaDTO model)
+        {
+            SocialMediaDTO newmodel = new SocialMediaDTO();
+            return View(newmodel);
+        }
+    }
+}
